c1510c462913b1f37438b468246171df06a978a4
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-07 20:22:25 +0530

    change ln loggers decoration

--- logger_ln.go
+++ logger_ln.go
@@ -4,58 +4,63 @@ import (
 	"fmt"
 	"log"
 	"runtime"
+
+	"github.com/fatih/color"
 )
 
 // Debug - log debugging messages
 func Debug(message interface{}) {
-	var fileInfo string = ""
+	debug := color.New(color.FgCyan).SprintfFunc()
+	var fileInfo string = "<nil>"
 
 	_, file, lineNum, ok := runtime.Caller(1)
 
 	if ok {
-		fileInfo = fmt.Sprintf(NoticeColor, fmt.Sprintf("%s::%d", file, lineNum))
+		fileInfo = fmt.Sprintf("%s::%d", file, lineNum)
 	}
 
 	if isDebugLog(GetLogLevel()) {
-		formatter := fmt.Sprintf("%s %s", fileInfo, fmt.Sprintf(DebugColor, message))
-		log.Println(formatter)
+		log.Println(debug("[DEBUG] %s %s", fileInfo, message))
 	}
 }
 
 // Info - log informative messages
 func Info(message interface{}) {
+	info := color.New(color.FgBlue).SprintfFunc()
+
 	if isInfoLog(GetLogLevel()) {
-		formatter := fmt.Sprintf(InfoColor, message)
-		log.Println(formatter)
+		log.Println(info("[INFO] %v", message))
 	}
 }
 
 // Warn - log warning messages
 func Warn(message interface{}) {
+	warn := color.New(color.FgBlack).Add(color.BgYellow).SprintfFunc()
+
 	if isWarnLog(GetLogLevel()) {
-		formatter := fmt.Sprintf(WarningColor, message)
-		log.Println(formatter)
+		log.Println(warn("[WARNING] %v", message))
 	}
 }
 
 // Success - log success messages
 func Success(message interface{}) {
+	success := color.New(color.FgBlack).Add(color.BgGreen).SprintfFunc()
+
 	if isSuccessLog(GetLogLevel()) {
-		formatter := fmt.Sprintf(SuccessColor, message)
-		log.Println(formatter)
+		log.Println(success("[SUCCESS] %v", message))
 	}
 }
 
 // Error - log error messages
 func Error(message interface{}) {
+	error := color.New(color.FgRed).Add(color.Bold).SprintfFunc()
+
 	if isErrorLog(GetLogLevel()) {
-		formatter := fmt.Sprintf(ErrorColor, message)
-		log.Fatal(formatter)
+		log.Fatal(error("[ERROR] %v", message))
 	}
 }
 
 // Println - print logs without any constraints
 func Println(message interface{}) {
-	formatter := fmt.Sprintf("%v", message)
-	log.Println(formatter)
+	log.Println(fmt.Sprintf("%v", message))
 }
